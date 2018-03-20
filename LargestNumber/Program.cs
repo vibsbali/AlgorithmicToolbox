4b00be93d28ccc10f44676a75bccb8266a436788
Vaibhav Bali <vibsbali@gmail.com>
2018-03-20 23:09:41 +1100

    Added largest number and min refills

---
+++ LargestNumber/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = args.First();
+            var hash = new Dictionary<int, int>();
+            for (int i = 0; i < number.Length; i++)
+            {
+                var numberToAdd = int.Parse(number[i].ToString());
+                if (hash.ContainsKey(numberToAdd))
+                {
+                    hash[numberToAdd] += 1;
+                }
+                else
+                {
+                    hash.Add(numberToAdd, 1);
+                }
+            }
+
+            var largestNumberAsString = "";
+            for (int i = 9; i >= 0; i--)
+            {
+                if (hash.ContainsKey(i))
+                {
+                    for (int j = 0; j < hash[i]; j++)
+                    {
+                        largestNumberAsString += i;
+                    }
+                }
+            }
+
+            var finalSalary = decimal.Parse(largestNumberAsString);
+            Console.WriteLine(finalSalary);
+        }
+    }
+}
---
+++ MinimumRefill/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MinimumRefill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var maxDist = 400;
+            var distanceTraveledSoFar = 0;
+            var endpoint = 1155;
+
+            //assuming gasStations are in ascending order 
+            var gasStations = new int[] {200, 375, 550, 750};
+            List<int> stationsWhereRefill = new List<int>();
+
+            //cant reach first station
+            if (gasStations[0] > maxDist)
+            {
+                throw new ApplicationException("Cant reach!");
+            }
+
+            for (int i = 1; i < gasStations.Length; i++)
+            {
+                if (gasStations[i] > maxDist + distanceTraveledSoFar)
+                {
+                    //refill at previous here
+                    stationsWhereRefill.Add(gasStations[i-1]);
+                    distanceTraveledSoFar = gasStations[i-1];
+
+                    if (distanceTraveledSoFar + maxDist >= endpoint)
+                    {
+                        break;
+                    }
+                }
+
+                //very last
+                if (i == gasStations.Length - 1)
+                {
+                    stationsWhereRefill.Add(gasStations[i]);
+                    distanceTraveledSoFar = gasStations[i];
+                }
+            }
+
+            if (distanceTraveledSoFar + maxDist < endpoint)
+            {
+                throw new ApplicationException($"Cant reach! max distance travelled so far { distanceTraveledSoFar} and can reach upto {distanceTraveledSoFar + maxDist}");
+            }
+
+
+            Console.WriteLine("Refill at following kms");
+            foreach (var gasStation in stationsWhereRefill)
+            {   
+                Console.WriteLine(gasStation);
+            }
+        }
+    }
+}
